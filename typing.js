f048818bc8b1af46c3d0df455fb3a01bb7076c6a
Brett Fisher <bdf5106@psu.edu>
2017-12-04 14:10:40 -0500

    Initial commit

---
+++ typing.js
@@ -0,0 +1,6 @@
+var i = 0;
+var text = getElementById("name").innerHtml;
+var speed = Math.floor(Math.random() * 100) + 1;
+
+let doc = document.getElementById('content');
+doc.style.backgroundColor = 'red';
